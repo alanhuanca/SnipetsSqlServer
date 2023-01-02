bf08def18ef0eb691b7482582412f24285a7f60d
Alan Huanca <ahuanca@peruapps.com.pe>
2023-01-02 12:37:06 -0500

    se agrgeo script de feriados

--- Crear Feriados BigBank.sql
+++ Crear Feriados BigBank.sql
@@ -1,4 +1,4 @@
-select * from si_finferiado where year(dFeriado)=2023 and cDescFeriado!='Feriado Dom' order by dFeriado desc
+select * from feriados where year(dFeriado)=2023 and cDescFeriado!='Feriado Dom' order by dFeriado desc
 
 DECLARE @contador int =1
 DECLARE @fechaIni DATE='2023-10-29'
@@ -8,9 +8,9 @@ begin
 
 	SET @fechaFeriado=DATEADD(DAY,@contador*7,@fechaIni)
 	print @fechaFeriado
-	INSERT si_finferiado VALUES(@fechaFeriado,1,1,1,'Feriado Dom')
-	INSERT si_finferiado VALUES(@fechaFeriado,1,1,2,'Feriado Dom')
-	INSERT si_finferiado VALUES(@fechaFeriado,1,1,3,'Feriado Dom')
+	INSERT feriados VALUES(@fechaFeriado,1,1,1,'Feriado Dom')
+	INSERT feriados VALUES(@fechaFeriado,1,1,2,'Feriado Dom')
+	INSERT feriados VALUES(@fechaFeriado,1,1,3,'Feriado Dom')
 	set @contador=@contador+1
 end
 
@@ -18,19 +18,19 @@ go
 
 --Depende de que días se asigna la semana santa
 
-INSERT si_finferiado VALUES('2023-04-06',1,1,1,'Semana Santa')
-INSERT si_finferiado VALUES('2023-04-07',1,1,1,'Semana Santa')
+INSERT feriados VALUES('2023-04-06',1,1,1,'Semana Santa')
+INSERT feriados VALUES('2023-04-07',1,1,1,'Semana Santa')
 
-INSERT si_finferiado VALUES('2023-04-06',1,1,2,'Semana Santa')
-INSERT si_finferiado VALUES('2023-04-07',1,1,2,'Semana Santa')
+INSERT feriados VALUES('2023-04-06',1,1,2,'Semana Santa')
+INSERT feriados VALUES('2023-04-07',1,1,2,'Semana Santa')
 
-INSERT si_finferiado VALUES('2023-04-06',1,1,3,'Semana Santa')
-INSERT si_finferiado VALUES('2023-04-07',1,1,3,'Semana Santa')
+INSERT feriados VALUES('2023-04-06',1,1,3,'Semana Santa')
+INSERT feriados VALUES('2023-04-07',1,1,3,'Semana Santa')
 
 go
 
 
-DECLARE @tFeriadoTemp TABLE(feriadoTemp DATE, idTipoFeriado INT, lVigente INT, idAgencia INT , cDescFeriado varchar(150))
+DECLARE @tFeriadoTemp TABLE(feriadoTemp DATE, idTipoFeriado INT, Vigente INT, idAgencia INT , DescFeriado varchar(150))
 
 INSERT @tFeriadoTemp VALUES('2023-05-01',1,1,1,'Día del trabajador')
 INSERT @tFeriadoTemp VALUES('2023-06-29',1,1,1,'San Pedro')
@@ -43,54 +43,54 @@ INSERT @tFeriadoTemp VALUES('2023-12-08',1,1,1,'Inmaculada Concepción')
 INSERT @tFeriadoTemp VALUES('2023-12-25',1,1,1,'Navidad') 
 
 
-DECLARE @feriadoTemp DATE, @idTipoFeriado INT, @lVigente INT, @idAgencia INT , @cDescFeriado varchar(150)
+DECLARE @feriadoTemp DATE, @idTipoFeriado INT, @Vigente INT, @idAgencia INT , @DescFeriado varchar(150)
 
 DECLARE db_cursor CURSOR FOR 
 SELECT * FROM @tFeriadoTemp
 
 OPEN db_cursor  
-FETCH NEXT FROM db_cursor INTO @feriadoTemp,@idTipoFeriado,@lVigente,@idAgencia,@cDescFeriado 
+FETCH NEXT FROM db_cursor INTO @feriadoTemp,@idTipoFeriado,@Vigente,@idAgencia,@DescFeriado 
 
 WHILE @@FETCH_STATUS = 0  
 BEGIN  
 
-	IF NOT EXISTS(SELECT * FROM SI_FinFeriado WHERE dFeriado=@feriadoTemp)
+	IF NOT EXISTS(SELECT * FROM feriados WHERE dFeriado=@feriadoTemp)
 	BEGIN
-		INSERT si_finferiado VALUES(@feriadoTemp,@idTipoFeriado,@lVigente,1,@cDescFeriado )
-		INSERT si_finferiado VALUES(@feriadoTemp,@idTipoFeriado,@lVigente,2,@cDescFeriado )
-		INSERT si_finferiado VALUES(@feriadoTemp,@idTipoFeriado,@lVigente,3,@cDescFeriado )
+		INSERT feriados VALUES(@feriadoTemp,@idTipoFeriado,@Vigente,1,@DescFeriado )
+		INSERT feriados VALUES(@feriadoTemp,@idTipoFeriado,@Vigente,2,@DescFeriado )
+		INSERT feriados VALUES(@feriadoTemp,@idTipoFeriado,@Vigente,3,@DescFeriado )
 	END
 
-	IF NOT EXISTS(SELECT * FROM SI_FinFeriado WHERE dFeriado=dateadd(year,1,@feriadoTemp))
+	IF NOT EXISTS(SELECT * FROM feriados WHERE dFeriado=dateadd(year,1,@feriadoTemp))
 	BEGIN
-		INSERT si_finferiado VALUES(dateadd(year,1,@feriadoTemp),@idTipoFeriado,@lVigente,1,@cDescFeriado )
-		INSERT si_finferiado VALUES(dateadd(year,1,@feriadoTemp),@idTipoFeriado,@lVigente,2,@cDescFeriado )
-		INSERT si_finferiado VALUES(dateadd(year,1,@feriadoTemp),@idTipoFeriado,@lVigente,3,@cDescFeriado )
+		INSERT feriados VALUES(dateadd(year,1,@feriadoTemp),@idTipoFeriado,@Vigente,1,@DescFeriado )
+		INSERT feriados VALUES(dateadd(year,1,@feriadoTemp),@idTipoFeriado,@Vigente,2,@DescFeriado )
+		INSERT feriados VALUES(dateadd(year,1,@feriadoTemp),@idTipoFeriado,@Vigente,3,@DescFeriado )
 	END
 
-	IF NOT EXISTS(SELECT * FROM SI_FinFeriado WHERE dFeriado=dateadd(year,2,@feriadoTemp))
+	IF NOT EXISTS(SELECT * FROM feriados WHERE dFeriado=dateadd(year,2,@feriadoTemp))
 	BEGIN
-		INSERT si_finferiado VALUES(dateadd(year,2,@feriadoTemp),@idTipoFeriado,@lVigente,1,@cDescFeriado )
-		INSERT si_finferiado VALUES(dateadd(year,2,@feriadoTemp),@idTipoFeriado,@lVigente,2,@cDescFeriado )
-		INSERT si_finferiado VALUES(dateadd(year,2,@feriadoTemp),@idTipoFeriado,@lVigente,3,@cDescFeriado )
+		INSERT feriados VALUES(dateadd(year,2,@feriadoTemp),@idTipoFeriado,@Vigente,1,@DescFeriado )
+		INSERT feriados VALUES(dateadd(year,2,@feriadoTemp),@idTipoFeriado,@Vigente,2,@DescFeriado )
+		INSERT feriados VALUES(dateadd(year,2,@feriadoTemp),@idTipoFeriado,@Vigente,3,@DescFeriado )
 	END
 
-	IF NOT EXISTS(SELECT * FROM SI_FinFeriado WHERE dFeriado=dateadd(year,3,@feriadoTemp))
+	IF NOT EXISTS(SELECT * FROM feriados WHERE dFeriado=dateadd(year,3,@feriadoTemp))
 	BEGIN
-		INSERT si_finferiado VALUES(dateadd(year,3,@feriadoTemp),@idTipoFeriado,@lVigente,1,@cDescFeriado )
-		INSERT si_finferiado VALUES(dateadd(year,3,@feriadoTemp),@idTipoFeriado,@lVigente,2,@cDescFeriado )
-		INSERT si_finferiado VALUES(dateadd(year,3,@feriadoTemp),@idTipoFeriado,@lVigente,3,@cDescFeriado )
+		INSERT feriados VALUES(dateadd(year,3,@feriadoTemp),@idTipoFeriado,@Vigente,1,@DescFeriado )
+		INSERT feriados VALUES(dateadd(year,3,@feriadoTemp),@idTipoFeriado,@Vigente,2,@DescFeriado )
+		INSERT feriados VALUES(dateadd(year,3,@feriadoTemp),@idTipoFeriado,@Vigente,3,@DescFeriado )
 	END
 
-	IF NOT EXISTS(SELECT * FROM SI_FinFeriado WHERE dFeriado=dateadd(year,4,@feriadoTemp))
+	IF NOT EXISTS(SELECT * FROM feriados WHERE dFeriado=dateadd(year,4,@feriadoTemp))
 	BEGIN
-		INSERT si_finferiado VALUES(dateadd(year,4,@feriadoTemp),@idTipoFeriado,@lVigente,1,@cDescFeriado )
-		INSERT si_finferiado VALUES(dateadd(year,4,@feriadoTemp),@idTipoFeriado,@lVigente,2,@cDescFeriado )
-		INSERT si_finferiado VALUES(dateadd(year,4,@feriadoTemp),@idTipoFeriado,@lVigente,3,@cDescFeriado )
+		INSERT feriados VALUES(dateadd(year,4,@feriadoTemp),@idTipoFeriado,@Vigente,1,@DescFeriado )
+		INSERT feriados VALUES(dateadd(year,4,@feriadoTemp),@idTipoFeriado,@Vigente,2,@DescFeriado )
+		INSERT feriados VALUES(dateadd(year,4,@feriadoTemp),@idTipoFeriado,@Vigente,3,@DescFeriado )
 	END
 	      
 	  	   
-	FETCH NEXT FROM db_cursor INTO @feriadoTemp,@idTipoFeriado,@lVigente,@idAgencia,@cDescFeriado  
+	FETCH NEXT FROM db_cursor INTO @feriadoTemp,@idTipoFeriado,@Vigente,@idAgencia,@DescFeriado  
 END 
 
 CLOSE db_cursor  
